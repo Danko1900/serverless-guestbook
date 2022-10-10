58841ebb2f314bf5792d12d5dad9c5466cec97b2
Danko1900 <108917243+Danko1900@users.noreply.github.com>
2022-10-10 00:42:54 -0400

    Update Syntax

--- guestbook.js
+++ guestbook.js
@@ -7,7 +7,7 @@ const guestbook = {
   get() {
     return $.ajax({
       type: 'GET',
-      url: '${apiURL}/read-guestbook-entries-sequence.json',
+      url: `${apiURL}/read-guestbook-entries-sequence.json`,
       dataType: 'json'
     });
   },
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: '${apiURL}/save-guestbook-entry-sequence.json',
+      url: `${apiURL}/save-guestbook-entry-sequence.json`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
